1d4138bae029a9d62a77ef12f53e365656a2c584
Rafael Souza <rafaelrochasouza@outlook.com.br>
2020-09-26 22:33:32 -0300

    Fixed flake8

--- src/api/routes/router.py
+++ src/api/routes/router.py
@@ -6,4 +6,5 @@ from src.api.routes import heartbeat, prediction
 api_router = APIRouter()
 
 api_router.include_router(heartbeat.router, tags=["health"], prefix="/health")
-api_router.include_router(prediction.router, tags=["prediction"], prefix="/model")
+api_router.include_router(prediction.router, tags=["prediction"],
+                          prefix="/model")
