3094b9d8b45b22bb7f335713727831c20e06e128
Rafael Souza <rafaelrochasouza@outlook.com.br>
2020-10-28 20:02:19 -0300

    Added CORS

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .api.routes.router import api_router
 from .core.config import API_PREFIX, APP_NAME, APP_VERSION, ENV
@@ -11,6 +12,13 @@ def app_setup() -> FastAPI:
     if ENV == "dev":
         debug = True
     api = FastAPI(title=APP_NAME, version=APP_VERSION, debug=debug)
+
+    api.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"])
+
     api.include_router(api_router, prefix=API_PREFIX)
 
     api.add_event_handler("startup", start_app_handler(api))
